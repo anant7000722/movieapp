d80300fc5d6afffd349cff33d30af72ff7ea578c
Anant Sharma <anant@maropost.com>
2017-12-19 19:12:42 +0530

    browse more

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,52 +1,50 @@
 class MoviesController < ApplicationController
-	before_action :set_id, except:[:new,:create,:index,:show]
+	before_action :set_id, except:[:new,:create,:index,:show,:moviesbynav]
 
 
 	def index
-    if params[:search].present?
-      @movies = Movie.where(['title like ?', "%"+params[:search]+"%"])
-    else
-      @movies = Movie.all
+	    if params[:search].present?
+	      @movies = Movie.where(['title like ?', "%"+params[:search]+"%"])
+	    else
+	      @movies = Movie.all
+	    end
+		@movies_sorted = @movies.order('rating DESC').limit(4)
+    	@movies_viewed = @movies.order('impression_count DESC').limit(4)
     end
+
+    def moviesbynav
+
+    	 if params[:search].present?
+	      @movies = Movie.where(['title like ?', "%"+params[:search]+"%"])
+	    else
+	      @movies = Movie.all
+	    end
 		@movies_sorted = @movies.order('rating DESC')
-    @movies_viewed = @movies.order('impression_count DESC')
-    
-	end
+    	@movies_viewed = @movies.order('impression_count DESC')
+
+    end
 
 	def new
-		 @movies = Movie.new
+		@movies = Movie.new
 	end
 
 	def create
 		@movies =  Movie.new(movie_params)
-	  if @movies.save
-	   redirect_to @movies,notice: "movie Successfully Saved"
-	  else
-	   render 'new'
-	 end
+		if @movies.save
+		   redirect_to @movies,notice: "movie Successfully Saved"
+		else
+		   render 'new'
+		end
 	end
 
 	def show
 		@movies1 = Movie.all
-    @movies = Movie.find(params[:id])
+    	@movies = Movie.find(params[:id])
     
-    @i = Impression.create(movie_id: @movies.id)
-  	@movies_sorted = @movies1.order('rating DESC')
+    	@i = Impression.create(movie_id: @movies.id)
+  		@movies_sorted = @movies1.order('rating DESC')
 
-		
 		@reviews = Review.where(movie_id: @movies.id).order("created_at DESC")
-	
-	end
-
-
-	def editM
-	end
-
-	def update
-	end
-
-
-	def destroy
 	end
 
 	private
@@ -54,9 +52,9 @@ class MoviesController < ApplicationController
   	 params.require(:movie).permit(:title,:image,:genre,:plot,:rating,:web, :year, :cast)
     end
  
-  def set_id
-    @movies = Movie.find(params[:id])
-  end
+  	def set_id
+   		@movies = Movie.find(params[:id])
+  	end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  	get "movies/moviesbynav" 
+  
   	devise_for :users
   	
 	resources :movies do
 		resources :reviews, except: [:show, :index]
   	end
   root 'movies#index'
-  
+
 end
