4aa20f5fda1f2b9896314b5ab1f20f96b76593ca
Anant Sharma <anant@maropost.com>
2017-12-29 16:52:07 +0530

    adding things from dev.rb to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,23 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+   
+
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'anants70000007@gmail.com',
+  password:             'gorailsrailsguide',
+  authentication:       'plain',
+  enable_starttls_auto: true  
+}
+  config.action_mailer.perform_caching = false
+
+
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
@@ -88,4 +105,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'movieappanant.herokuapp.com' }
 end
