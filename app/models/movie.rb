c4bff1f715988054e9eb6aa9f6746055c566a5a1
Anant Sharma <anant@maropost.com>
2017-12-29 18:54:10 +0530

    ded

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
 	mount_uploader :image, MovieUploader
 	validates_presence_of :title, :genre, :plot, :image, :rating, :web, :cast
    
-    has_many :reviews
-    has_many :impressions
+    has_many :reviews, dependent: :destroy
+    has_many :impressions, dependent: :destroy
 
 end
